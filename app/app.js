2a56241fd0c8548c8673249be26a6fbfb2bb2c16
unknown <harvianto@hotmail.com>
2015-10-01 12:59:10 +0700

    asd

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ angular.module('myApp', [
             requireBase: false
         });*/
         $httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.withCredentials = true;
+        //$httpProvider.defaults.withCredentials = true;
         delete $httpProvider.defaults.headers.common["X-Requested-With"];
         $httpProvider.defaults.headers.common["Accept"] = "application/json";
         $httpProvider.defaults.headers.common["Content-Type"] = "application/json";

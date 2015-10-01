60bf10fb8ce3b72f8454a1d28403a122dd8ac681
unknown <harvianto@hotmail.com>
2015-10-01 12:42:00 +0700

    asd

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
-app.all('*', function(req, res, next) {
+/*app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    if ('OPTIONS' == req.method){
-        return res.send(200);
-    }
-    next();
-});
+if ('OPTIONS' == req.method){
+    return res.send(200);
+}
+next();
+});*/
 app.listen(process.env.PORT || 80);
